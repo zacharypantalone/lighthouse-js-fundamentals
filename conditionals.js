65b5177c8e02e92b5a7546de2dfe6663ea56ef14
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-02-03 16:23:02 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("Your are eligible ");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea");
+} 
+
+// if (!raining) {
+  //console.log("Leave your umbrella at home!");
+//} 
