ef76920fdf2bef01f7d2cf91b3d131b9bfaadce3
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-04-09 09:35:21 -0400

    packing list changes

---
+++ packingList.js
@@ -0,0 +1,18 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+
+
+
+//This is the for loop version
+/*for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+} */
+
